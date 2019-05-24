de38fed59313017564e9161da5b39db2bbc63940
Janczar Kurek <janczar.kurek@student.uj.edu.pl>
2019-05-24 21:03:06 +0200

    Added utility to compile gifs into bash scripts
    Signed-off-by: Janczar Kurek <janczar.kurek@student.uj.edu.pl>

---
+++ compile-gif.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function __cleanup {
+  rm *.gif.???
+  rm temp.gif
+  exit 0
+}
+
+function __help {
+  echo $0
+}
+
+function __check_req {
+  if ! command -v $1 > /dev/null 2>&1; then
+    echo "Command $1 required but not installed, aborting."
+    exit 1
+  fi
+}
+
+__check_req gifsicle
+__check_req img2txt
+
+trap __cleanup SIGINT
+
+gifsicle --unoptimize $1 > temp.gif
+gifsicle --explode temp.gif
+
+RESULT_FILE="result.sh"
+rm -f $RESULT_FILE
+echo "clear" >> $RESULT_FILE
+echo "while true;" >> $RESULT_FILE
+echo "do" >> $RESULT_FILE
+
+for frame in temp.gif.???
+do
+  echo "tput cup 0 0" >> $RESULT_FILE
+  echo 'echo "' >> $RESULT_FILE
+  img2txt $frame >> $RESULT_FILE
+  echo '"' >> $RESULT_FILE
+  echo "sleep 0.06" >> $RESULT_FILE
+done
+
+echo "done" >> $RESULT_FILE
+
+__cleanup
